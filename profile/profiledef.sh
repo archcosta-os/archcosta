3350e3336db73b795653c72543f5a3a3932c8a99
costaOSS <jignesh23@proton.me>
2026-03-19 16:57:54 +0530

    [profile] enable GUI autologin and resolve CLI setup issue

--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -13,7 +13,14 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
+  ["/etc/passwd"]="0:0:644"
+  ["/etc/group"]="0:0:644"
+  ["/etc/hostname"]="0:0:644"
+  ["/etc/locale.conf"]="0:0:644"
+  ["/etc/machine-id"]="0:0:444"
+  ["/etc/lightdm/lightdm.conf"]="0:0:644"
   ["/root"]="0:0:750"
+  ["/home/live"]="1000:1000:750"
   ["/usr/local/bin/archcosta-installer"]="0:0:755"
   ["/usr/local/bin/archcosta-live-setup"]="0:0:755"
 )
