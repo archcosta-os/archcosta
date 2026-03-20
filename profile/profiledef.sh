c2337eda3ed0a359a05bf80974576a49a90919d4
costaOSS <jignesh23@proton.me>
2026-03-20 17:11:55 +0530

    [branding] revert to ArchCosta (remove Spark)

--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 iso_name="archcosta"
-iso_label="ARCHCOSTA_SPARK"
+iso_label="ARCHCOSTA"
 iso_publisher="ArchCosta <https://archcosta-os.github.io>"
-iso_application="ArchCosta Linux Spark"
-iso_version="Spark"
+iso_application="ArchCosta Linux"
+iso_version="rolling"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux' 'uefi.grub')
@@ -13,14 +13,7 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
-  ["/etc/passwd"]="0:0:644"
-  ["/etc/group"]="0:0:644"
-  ["/etc/hostname"]="0:0:644"
-  ["/etc/locale.conf"]="0:0:644"
-  ["/etc/machine-id"]="0:0:444"
-  ["/etc/lightdm/lightdm.conf"]="0:0:644"
   ["/root"]="0:0:750"
-  ["/home/live"]="1000:1000:750"
   ["/usr/local/bin/archcosta-installer"]="0:0:755"
   ["/usr/local/bin/archcosta-live-setup"]="0:0:755"
 )
