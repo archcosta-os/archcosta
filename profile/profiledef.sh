c7aeffd692a2594c65ebdb2264a78eb486ea4453
costaOSS <jignesh23@proton.me>
2026-03-22 12:06:50 +0530

    [profile] cleanup Linexin features and revert to standard Calamares
    - Keep: linexin-center, linexin-updater, linexin-desktop-presets
    - Keep: sound fix and livecd-sound scripts
    - Add: calamares and calamares-settings-arch
    - Remove: linexin-repo from pacman.conf
    - Update: archcosta-installer script to launch Calamares

--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 iso_name="archcosta"
-iso_label="ARCHCOSTA"
-iso_publisher="ArchCosta <https://archcosta-os.github.io>"
-iso_application="ArchCosta Linux"
+iso_label="ARCHCOSTA_$(date +%Y%m)"
+iso_publisher="ArchCosta OS <https://archcosta-os.github.io>"
+iso_application="ArchCosta Live/Rescue"
 iso_version="rolling"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux' 'uefi.grub')
+bootmodes=("bios.syslinux.mbr" "bios.syslinux.eltorito" "uefi-x64.systemd-boot.esp")
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
-  ["/usr/local/bin/linexin-sound-fix-usb-dac"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/usr/local/bin/archcosta-installer"]="0:0:755"
   ["/usr/local/bin/archcosta-live-setup"]="0:0:755"
+  ["/usr/local/bin/linexin-sound-fix-usb-dac"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
 )
